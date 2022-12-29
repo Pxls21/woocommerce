04213bf49a9d2303792a772d079272c4b65ad936
Parabeac-Nest <admin@parabeac.com>
2022-12-29 19:36:48 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/popgrape_text_styles.g.dart
@@ -0,0 +1,21 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class PopgrapeTextStyles {
+/// Previous style:
+body/large/semibold
+static const TextStyle bodyLargeSemibold = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0.20000000298023224,
+  fontFamily: 'SF Pro Display',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}
